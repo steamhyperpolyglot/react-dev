3b336f828321cd875f855fba81ed17457fa05d1b
Sherman <sherman@thecxhacker.guru>
2019-06-23 04:19:06 +0800

    Updated the to-do sample application

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,30 +4,56 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 	
-	constructor(props) {
-		super(props);
+	constructor ( props ) {
+		super ( props );
 		this.state = {
-			userName: "Sherman"
+			userName: "Sherman",
+			todoItems: [
+				{ action: "Buy Flowers", done: false },
+				{ action: "Get Shoes", done: false },
+				{ action: "Collect Tickets", done: true },
+				{ action: "Call Joe", done: false }
+			],
+			newItemText: ""
 		}
 	}
 	
-	changeStateData = () => {
-		this.setState({
-			userName: this.state.userName === "Sherman" ? "Dan" : "Sherman"
-		})
+	updateNewTextValue = ( event ) => {
+		this.setState ( { newItemText: event.target.value } );
 	};
 	
-	render() {
-		return (
-			<div>
-				<h4 className="bg-primary text-white text-center p-2">
-					{ this.state.userName }'s To Do List
-				</h4>
-				<button className="btn btn-primary m-2"
-				        onClick={ this.changeStateData }>
-					Change
-				</button>
-			</div>
-		)
+	createNewTodo = () => {
+		if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+			this.setState({
+				todoItems: [...this.state.todoItems,
+					{ action: this.state.newItemText, done: false }],
+				newItemText: ""
+			});
+		}
 	};
+	
+	changeStateData = () => {
+		this.setState ( {
+			userName: this.state.userName === "Sherman" ? "Dan" : "Sherman"
+		} )
+	};
+	
+	// Using a fat arrow function.
+	render = () =>
+		<div>
+			<h4 className="bg-primary text-white text-center p-2">
+				{ this.state.userName }'s To Do List
+				({ this.state.todoItems.filter(t => !t.done).length } items to do)
+			</h4>
+			<div className="container-fluid">
+				<div className="my-1">
+					<input className="form-control"
+					       value={this.state.newItemText}
+					       onChange={this.updateNewTextValue} />
+					<button className="btn btn-primary mt-1"
+					        onClick={ this.createNewTodo }>Add</button>
+				</div>
+			</div>
+		</div>
+	
 }
