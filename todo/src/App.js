9ef926039101003a52b447bbd3e6aa875f0632bc
Sherman Chen <sherman@thecxhacker.guru>
2019-06-23 17:33:28 +0800

    Completed the todo app for Chapter 1

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
+import { VisibilityControl } from "./VisibilityControl";
 
 export default class App extends Component {
 	
@@ -14,45 +16,86 @@ export default class App extends Component {
 				{ action: "Collect Tickets", done: true },
 				{ action: "Call Joe", done: false }
 			],
-			newItemText: ""
+			showCompleted: false
 		}
 	}
 	
-	updateNewTextValue = ( event ) => {
+	/*updateNewTextValue = ( event ) => {
 		this.setState ( { newItemText: event.target.value } );
-	};
+	};*/
 	
-	createNewTodo = () => {
-		if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+	createNewTodo = (task) => {
+		if (!this.state.todoItems.find(item => item.action === task)) {
 			this.setState({
-				todoItems: [...this.state.todoItems,
-					{ action: this.state.newItemText, done: false }],
-				newItemText: ""
-			});
+				todoItems: [...this.state.todoItems, { action: task, done: false }]
+			},
+			() => localStorage.setItem("todos", JSON.stringify(this.state)));
 		}
 	};
 	
-	changeStateData = () => {
+	toggleTodo = ( todo ) => this.setState ( {
+		todoItems: this.state.todoItems.map ( item => item.action === todo.action ?
+			{ ...item, done: !item.done } : item )
+	} );
+	
+	todoTableRows = (doneValue) => this.state.todoItems
+		.filter(item => item.done === doneValue).map ( item =>
+		<TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />
+	);
+	
+	/**
+	 * The componentDidMount method is always called early in the component's life.
+	 * This is a great place to load our data.
+	 */
+	componentDidMount = () => {
+		let data = localStorage.getItem("todos");
+		this.setState(data != null ? JSON.parse(data) : {
+			userName: "Sherman",
+			todoItems: [
+				{ action: "Buy Flowers", done: false },
+				{ action: "Get Shoes", done: false },
+				{ action: "Collect Tickets", done: true },
+				{ action: "Call Joe", done: false }
+				]
+		});
+	};
+	
+	/*changeStateData = () => {
 		this.setState ( {
 			userName: this.state.userName === "Sherman" ? "Dan" : "Sherman"
 		} )
-	};
+	};*/
 	
 	// Using a fat arrow function.
 	render = () =>
 		<div>
-			<h4 className="bg-primary text-white text-center p-2">
-				{ this.state.userName }'s To Do List
-				({ this.state.todoItems.filter(t => !t.done).length } items to do)
-			</h4>
+			<TodoBanner name={ this.state.userName } tasks={ this.state.todoItems } />
 			<div className="container-fluid">
-				<div className="my-1">
-					<input className="form-control"
-					       value={this.state.newItemText}
-					       onChange={this.updateNewTextValue} />
-					<button className="btn btn-primary mt-1"
-					        onClick={ this.createNewTodo }>Add</button>
-				</div>
+				<TodoCreator callback={ this.createNewTodo } />
+				<table className="table table-striped table-bordered">
+					<thead>
+					<tr>
+						<th>Description</th>
+						<th>Done</th>
+					</tr>
+					</thead>
+					<tbody>{ this.todoTableRows (false) }</tbody>
+					<div className="bg-secondary text-white text-center p-2">
+						<VisibilityControl description="Completed Tasks"
+						                   isChecked={ this.state.showCompleted }
+						                   callback={ (checked) =>
+							                   this.setState({ showCompleted: checked })} />
+					</div>
+					{
+						this.state.showCompleted &&
+						<table className="table table-striped table-bordered">
+							<thead>
+								<tr><th>Description</th><th>Done</th></tr>
+							</thead>
+							<tbody>{ this.todoTableRows(true) }</tbody>
+						</table>
+					}
+				</table>
 			</div>
 		</div>
 	
