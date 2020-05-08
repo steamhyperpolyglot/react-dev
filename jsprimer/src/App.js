8fc20fe73f5ec892299e6660a0bcd0e3a4efaa9f
Sherman Chen <uxdailygeek@gmail.com>
2020-05-08 18:40:54 +0800

    Finished the code examples for jsprimer

--- jsprimer/src/App.js
+++ jsprimer/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import sumOdds, { sumValues } from "./Modules/sum";
-import { multiply, subtract } from "./Modules/Operations";
+import { asyncAdd } from "./async"
+// import sumOdds, { sumValues } from "./Modules/sum";
+// import { multiply, subtract } from "./Modules/Operations";
+//
+// let values = [10, 20, 30, 40, 50];
+//
+// let total = sumValues(values);
+// let odds = sumOdds(values);
+//
+// console.log(`Total: ${total}, Odd Total: ${odds}`);
+// console.log(`Multiply: ${multiply(values)}`);
 
 let values = [10, 20, 30, 40, 50];
 
-let total = sumValues(values);
-let odds = sumOdds(values);
+// asyncAdd(values).then(total => console.log(`Main Total: ${total}`));
 
-console.log(`Total: ${total}, Odd Total: ${odds}`);
-console.log(`Multiply: ${multiply(values)}`);
+async function doTask() {
+	let total = await asyncAdd(values);
+    console.log(`Main Total: ${total}`);
+}
 
+doTask();
 
 function App() {
   return (
